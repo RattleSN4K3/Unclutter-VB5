2e992144fdf232e9e5795a7717bee2f89a40b320
RattleSN4K3 <RattleSN4K3@gmail.com>
2017-04-16 19:16:33 +0200

    Fixing syntax errors and code compliance

--- Unclutter_VB5.user.js
+++ Unclutter_VB5.user.js
@@ -7,6 +7,7 @@
 // @require     https://code.jquery.com/jquery-1.11.0.min.js
 // @require     https://cdnjs.cloudflare.com/ajax/libs/moment.js/1.7.2/moment.min.js
 // @version     0
+// @author      RattleSN4K3
 // @grant       none
 // @run-at      document-start
 // ==/UserScript==
@@ -36,43 +37,44 @@ function mainProc()
 {
   if (typeof jQuery == 'undefined') {
     console.log("no jquery. wait for it...");
-    return;    
+    return;
   }
-    
+
   // mutex
   if (IsProcessed) return;
   IsProcessed = true;
-  
+
   console.log("Unclutter VB5 loaded...");
-  
+
   var topbar = $('#channel-tabbar');
-  if (topbar == undefined || topbar == null) {
+  if (topbar === undefined || topbar === null || !topbar.length) {
     console.log("No top bar");
     return;
   }
 
   var subbar = $('#channel-subtabbar');
-  if (subbar == undefined || subbar == null) {
+  if (subbar === undefined || subbar === null || !subbar.length) {
     console.log("No sub bar");
-    return; 
+    return;
   }
-  
+
   var mainbar = $('#main-navbar');
+  var bread = $('#breadcrumbs');
   console.log(topbar);
   console.log(subbar);
   console.log(mainbar);
 
-  
+
   // Move main-bar search into channel bar
   $('#header > .toolbar > ul > li.search-container').css('margin', '0px');
   $('#header > .toolbar > ul').detach().appendTo(topbar);
-  
+
 
   $('ul.secondary-nav', mainbar).addClass('channel-subtabbar-list');
   $('ul.secondary-nav', mainbar).addClass('js-channel-subtabbar-list');
   $('ul.secondary-nav', mainbar).detach().appendTo(subbar);
 
-  
+
   // add profile link
   $('.notifications-container').before('<li class="welcomelink">Welcome, <a href="'+window.profileUrl+'">'+window.userName+'</a></li>');
 
@@ -82,23 +84,23 @@ function mainProc()
   //$('#notifications').before('<li><a href="'+window.PROFILEURL+'">My Profile</a></li>');
 
   // add register/login for guests
-  if (window.pageData == undefined || window.pageData.userid == undefined || window.pageData.userid == '0') {
+  if (window.pageData === undefined || window.pageData.userid === undefined || window.pageData.userid == '0') {
     var nouserlist = $('<ul></ul>')
     .addClass('nouser')
     .addClass('h-right')
     .addClass('channel-subtabbar-list')
     .addClass('js-channel-subtabbar-list');
 
-    if (window.signInLinks != undefined && window.signInLinks.length > 0) {
+    if (window.signInLinks !== undefined && window.signInLinks.length > 0) {
       var registerurl = window.signInLinks[0].href;
-      nouserlist.append('<li><a href="'+registerurl+'" rel="nofollow" class="notreg">Register</a></li>')
-      nouserlist.append('<li>or</li>')
+      nouserlist.append('<li><a href="'+registerurl+'" rel="nofollow" class="notreg">Register</a></li>');
+      nouserlist.append('<li>or</li>');
     }
-    nouserlist.append('<li><a href="'+window.loginUrl+'" rel="nofollow" class="notreg">Sign in</a></li>')
+    nouserlist.append('<li><a href="'+window.loginUrl+'" rel="nofollow" class="notreg">Sign in</a></li>');
     nouserlist.appendTo(subbar);
   }
-  
-  
+
+
   // sub-forum list into main forum entry
   $('.subforum-list').each(function(index_sub, subforum){
     var mainforum = $(subforum).prev('tr');
@@ -109,24 +111,24 @@ function mainProc()
       //subitm.unwrap();
       subdiv.append(subitm);
     });
-    $(subforum).remove();    
+    $(subforum).remove();
     subdiv.css('margin-top', '15px');
     $('.forum-info', mainforum).append(subdiv);
-    
+
     */
-    
+
     // Somehow good
     //$('.forum-info', mainforum).append($('tr td div', subforum));
     //$(subforum).remove();
-    
-    var subformul = $('<ul style="margin-top: 10px;" />')
+
+    var subformul = $('<ul style="margin-top: 10px;" />');
     $('tr td div', subforum).each(function(index_itm, subitm){
       $(subitm).css('width', '-moz-fit-content');
       subformul.append($('<li style="display: inline; float: left; margin-left: 10px;" />').append(subitm));
     });
     $('.forum-info', mainforum).append(subformul);
     $(subforum).remove();
-    
+
 
     //$('.forum-info', mainforum).append($('td', subforum));
 
@@ -135,9 +137,9 @@ function mainProc()
     //var libutton = $('<li></li>').append($(this));
     //$('ul', topicbar).first().append(libutton);
   });
-  
-  
-  // 
+
+
+  // move post options ("see more", "goto post") into post controls bar
   $('li.b-post').each(function(index_post, post) {
     var postlinks = $('.post-links', post);
     var postli = $('<li class="h-margin-top-m" />');
@@ -145,15 +147,15 @@ function mainProc()
     //postlinks.detach().appendTo($('<li class="h-margin-top-m" />').append($('ul.h-left', post))) ;
     postlinks.detach().appendTo(postli);
     $('ul.h-left', post).append(postli);
-    
+
     //postlinks.detach().appendTo(.remove();
   });
-  
-  
+
+
   var threadbar = $('#thread-view-tab .conversation-toolbar');
-  if (threadbar != null && threadbar.length) {
+  if (threadbar !== null && threadbar.length) {
     console.log("change thread bar");
-    
+
     // check or create toolset
     var toolset = null;
     var toolsetcheck = $('ul.toolset-left', threadbar);
@@ -170,9 +172,9 @@ function mainProc()
     } else {
       console.log("what to do with toolset");
     }
-      
-    if (toolset != null) {
-    
+
+    if (toolset !== null) {
+
       // move subscribe button in thread toolset section
       $('.conversation-controls button').each(function(){
         var libutton = $('<li></li>').append($(this));
@@ -181,7 +183,7 @@ function mainProc()
 
 
       // add thread into thread bar
-      var threadli = $('<li></li>')
+      var threadli = $('<li></li>');
       //threadli.addClass('ui-widget-header');
       var threaddiv = GetTitleDiv();// $('div.module-title').last();
       threaddiv.removeClass('module-title');
@@ -190,7 +192,7 @@ function mainProc()
       threada[0].style.setProperty('background-color', 'inherit', 'important');
       $('h1', threaddiv).wrapInner(threada);
       threadli.append(threaddiv);
-      
+
       toolset.append(threadli);
       /*var threadli = $('<li></li>')
       var threadtitle = $('.module-title h1.main-title').last().text();
@@ -203,7 +205,7 @@ function mainProc()
       threadlink.css('color', 'inherit');
       threadlink.css('background-color', 'inherit !important');
       $('.module-title h1.main-title').last().wrapInner(threadlink);
-      
+
       threadli.append(threadlink);
       toolset.append(threadli);
       $('.module-title h1.main-title').last().remove();
@@ -217,30 +219,29 @@ function mainProc()
       ;*/
     }
   }
-  
+
   var topicbar = $('#topic-tab .conversation-toolbar');
-  if (topicbar != null && topicbar.length) {
+  if (topicbar !== null && topicbar.length) {
     console.log("change topic bar");
-    
+
     // move subscribe button in topic toolset section
     $('.channel-controls button').each(function(){
       var libutton = $('<li></li>').append($(this));
       $('ul', topicbar).first().append(libutton);
     });
-    
+
     // move rss icon into breadcrumbs
     var rssa = $('div.module-title a');
-    var bread = $('#breadcrumbs');
     //$('<li class="vb-icon separator"></li>').appendTo(bread);
     //$('<li class="crumb ellipsis"></li>').append(bread);
     $('<li class="separator" />').appendTo(bread);
     rssa.appendTo(bread);
 
-    
+
     // move title into topic bar
     var topiccontrols = $('ul', topicbar).first();
     if (topiccontrols.length) {
-      var titleli = $('<li></li>')
+      var titleli = $('<li></li>');
       //titleli.addClass('ui-widget-header');
       var titlediv = $('div.module-title').last();
       titlediv.removeClass('module-title');
@@ -256,48 +257,47 @@ function mainProc()
       $('div.module-title').last().remove();
     }
   }
-  
+
   // move page title into breadcrumbs if not in thread/topic view
   var pagetitle = GetTitleDiv();
-  if (pagetitle != null && pagetitle.length) {
+  if (pagetitle !== null && pagetitle.length) {
     pagetitle = pagetitle.first();
-    
-    var bread = $('#breadcrumbs');
+
     if (!bread.is(':empty')) {
       $('<li class="vb-icon separator"></li>').appendTo(bread);
       $('<li class="crumb ellipsis"></li>').append($('h1', pagetitle).html()).appendTo(bread);
-    
+
       pagetitle.remove();
     } else {
       $('.widget-content .b-button.b-button--secondary').after($('h1', pagetitle));
       pagetitle.remove();
     }
   }
-  
+
   // increase editor height
   $('.cke_inner .cke_contents').each(function(index, editor) {
     console.log("editor too small");
     $(editor).css('height', '150px');
     /*if ($(editor).height() < 100) {
-      
+
       $(editor).css('height', $(editor).height() + 'px');
     }*/
   });
-  
-  
-  
+
+
+
   // thread likes
-  $('.votes-count').remove();    
-  
+  $('.votes-count').remove();
+
   // remove margin from thread bar
   $('.widget-content').css('margin-top', '0px');
 
   // remove header
   $('#header').remove();
-  
-  // fixing line wrapping of links in sub-bar 
+
+  // fixing line wrapping of links in sub-bar
   $('li > a', subbar).css('display', 'inline');
-  
+
   // fixing widget offset
   $('.b-module.canvas-widget').css('min-height', '0px');
   //$('.b-module.canvas-widget').css('margin-bottom', '0px');
@@ -311,7 +311,7 @@ function mainProc()
 }
 
 function momentProc() {
-  
+
   $('time').each(function(i, elem) {
     //console.log($(elem).html());
     var d = moment($(elem).html(), 'MM/DD/YYYY, hh:mm a');
@@ -324,7 +324,7 @@ function momentProc() {
       $(elem).html(d.format("YYYY-MM-DD HH:mm"));
     }
   });
-  
+
   $('div.post-date:not(:has("*")), span.post-date:not(:has("*")), span.date:not(:has("*"))').each(function(i, elem) {
     //console.log($(elem).html());
     var d = moment($(elem).html(), 'MM/DD/YYYY, hh:mm a');
@@ -337,8 +337,8 @@ function momentProc() {
       $(elem).html(d.format("YYYY-MM-DD HH:mm"));
     }
   });
-  
-  
+
+
   $('.profile-info-item:contains("Last Activity")').each(function(i, elem) {
     var d = moment($(elem).html(), '[Last Activity:] MM/DD/YYYY, hh:mm a');
     $(elem).html(d.local().format("[Last Activity:] YYYY-MM-DD HH:mm"));
@@ -354,10 +354,10 @@ function GetTitleDiv() {
   var returndiv = null;
   $('.b-module.page-title-widget').each(function(index, div) {
     if (!$(div).hasClass('announcement-widget')) {
-      if (returndiv == null) returndiv = $('.module-title', $(div));
+      if (returndiv === null) returndiv = $('.module-title', $(div));
     }
   });
-  
+
   return returndiv;
 }
 
